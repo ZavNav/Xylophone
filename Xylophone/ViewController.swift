c2afc25b5692337105085c512119b06bd68aa528
ZavNav <arka146@mail.ru>
2022-03-08 22:33:13 +0300

    Next

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,16 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        
+        playSound(note: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.2) {sender.alpha = 1}
     }
     
+    func playSound(note:String){
+        let url = Bundle.main.url(forResource: note, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+        
+    }
 }
 
